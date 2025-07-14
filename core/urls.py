d058a7beb420526f3daa5b309d0670ebad6f7a29
Mayank Rathi <137880416+rmayank-24@users.noreply.github.com>
2025-07-14 15:55:18 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UploadPDFView, upload_pdf_from_url, chat_with_pdf
+from .views import UploadPDFView, upload_pdf_from_url, chat_with_pdf,health_check
 
 urlpatterns = [
+    path('', health_check, name='health'),
     path('upload/', UploadPDFView.as_view(), name='upload'),
     path('upload_url/', upload_pdf_from_url, name='upload_url'),
     path('chat/', chat_with_pdf, name='chat'),
